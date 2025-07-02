48e6ffa044ec4d8d519492baf49865977999e21b
LapTQ <lap.tq4@gmail.com>
2025-07-02 11:49:09 +0900

    .

--- ultralytics/engine/model.py
+++ ultralytics/engine/model.py
@@ -785,8 +785,9 @@ class Model(torch.nn.Module):
         self.trainer = (trainer or self._smart_load("trainer"))(overrides=args, _callbacks=self.callbacks)
         if not args.get("resume"):  # manually set model only if not resuming
             self.trainer.model = self.trainer.get_model(weights=self.model if self.ckpt else None, cfg=self.model.yaml)
-            if hasattr(self.model, "fid_model"):
-                self.trainer.model.fid_model = self.model.fid_model
+            # for federated learning
+            if hasattr(self.model, "id_client"):
+                self.trainer.model.id_client = self.model.id_client
             self.model = self.trainer.model
 
         self.trainer.hub_session = self.session  # attach optional HUB session
